09d181bb5bef910f594ed91d2edaf46b5c7995c5
samjames88 <shatko@tut.by>
2021-06-10 11:18:57 +0300

    Optimization JS akk

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+const akk = document.querySelectorAll('.TEXT');
+const button = document.getElementsByClassName('button');
+const frameTxt = document.getElementsByClassName('frameTxt');
+
+
+
+window.onload = function() {
+frameTxt[0].style.fontSize = '16px';
+button[0].style.backgroundImage = 'url(img/closeBlack.svg)';
+
+frameTxt[4].style.fontSize = '16px';
+button[4].style.backgroundImage = 'url(img/closeBlack.svg)';
+
+frameTxt[8].style.fontSize = '16px';
+button[8].style.backgroundImage = 'url(img/closeBlack.svg)';
+}
+
+
+
+
+akk.forEach(function (btn,index) {
+	btn.onclick = function (e) {
+if (e.currentTarget.classList.contains('hide'))
+	{
+e.currentTarget.classList.remove('hide');
+frameTxt[index].style.fontSize = '16px';
+document.getElementsByClassName('frameTxt2')[index].style.fontSize = '16px';
+button[index].style.backgroundImage = 'url(img/closeBlack.svg)';
+	}
+else 
+	{
+e.currentTarget.classList.add('hide');
+frameTxt[index].style.fontSize = '0px';
+document.getElementsByClassName('frameTxt2')[index].style.fontSize = '0px';
+button[index].style.backgroundImage = 'url(img/openBlack.svg)';
+	}
+}
+
+btn.onmousedown = function (e) {
+if (e.currentTarget.classList.contains('hide'))
+button[index].style.backgroundImage = 'url(img/openOrange.svg)';
+else
+button[index].style.backgroundImage = 'url(img/closeOrange.svg)';
+}
+
+btn.onmouseup = function (e) {
+if (e.currentTarget.classList.contains('hide'))	
+button[index].style.backgroundImage = 'url(img/openBlack.svg)';
+else
+button[index].style.backgroundImage = 'url(img/closeBlack.svg)';
+}
+
+    
+})
+
+
